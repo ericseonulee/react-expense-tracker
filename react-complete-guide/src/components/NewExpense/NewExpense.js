44ebddd91e9e3396601fb0dc2b5de441535f13ae
Eric Lee <szl0094@auburn.edu>
2023-04-08 14:27:07 -0500

    conditional content

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isAdding, setIsAdding] = useState(false);
+
+  const onAddHandler = () => {
+    setIsAdding(true);
+  };
+
+  const onCancelAddHandler = () => {
+    setIsAdding(false);
+  };
+
   const saveExpenseDataHandler = (expenseDataInput) => {
     const expenseData = {
       ...expenseDataInput,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsAdding(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isAdding && <button onClick={onAddHandler}>Add New Expense</button>}
+      {isAdding && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={onCancelAddHandler}
+        />
+      )}
     </div>
   );
 };
